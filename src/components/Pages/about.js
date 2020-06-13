d6f94637d627e7cb84b18b94cec34b2d58f992b6
Takudzwa413 <takuman456@gmail.com>
2020-06-13 20:51:37 +0200

    Final project upload

---
+++ src/components/Pages/about.js
@@ -0,0 +1,13 @@
+import React,{Fragment} from 'react'
+
+const about = () => {
+    return (
+        <Fragment>
+            <h2>About this App</h2>
+            <p>App to search Github Users</p>
+            <p>Version 1.0.9</p>
+            
+        </Fragment>
+    );
+};
+export default about;
